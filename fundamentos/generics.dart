2c01a32fe125227ab33548af39e8157cb4a19126
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-02-22 08:34:37 -0300

    Generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,13 @@
+main(){
+  List<String> frutas = ['Pera', 'Uva', 'Maçã'];
+  frutas.add('Salada Mista');
+
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente':4299.00,
+    'vendedor':2500.00,
+    'estagiário':500.00,
+  };
+  print(salarios);
+}
