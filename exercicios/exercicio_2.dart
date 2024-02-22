7841d590754b99786f62f230d7398a1019838393
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-02-22 09:55:59 -0300

    Exercício #2

--- exercicios/exercicio_2.dart
+++ exercicios/exercicio_2.dart
@@ -1,16 +1,23 @@
 
 class Produto {
-  String nome = "";
-  double preco = 0;
+  String nome;
+  double preco;
 
+Produto(this.nome, this.preco);
+/*
+    Produto(String nome, double preco){
+      this.nome = nome;
+      this.preco = preco;
+    }
+ */
  
 }
 
 main(){
-  var p1 = new Produto();
-  p1.nome = 'livro';
-  p1.preco = 4.20;
-
-  print('Produto ${p1.nome} custa R\$${p1.preco}');
+  var p1 = Produto('canera', 5.99);
+  var p2 = Produto('Livro', 15.99);
+ 
+print('Produto ${p1.nome} custa R\$${p1.preco}');
+print('Produto ${p2.nome} custa R\$${p2.preco}');
 
 }
